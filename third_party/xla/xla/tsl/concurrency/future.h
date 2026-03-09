b6008806d72ada2bcf4185fc622eb7ef98a6f93b
Christian Sigg <csigg@google.com>
2026-03-09 08:52:39 -0700

    Merge cub_sort_kernel_cuda implementation and FFI registration.
    The split between the CUDA implementation and FFI registration for cub_sort_kernel_cuda was a workaround for NVCC versions prior to CUDA 12.4.1. This restriction is now lifted, allowing the code to be combined into a single compilation unit. The `cub_sort_kernel_cuda.cc` and `cub_sort_kernel_cuda_impl.cu.cc` files are merged, and the `cub_sort_kernel_cuda.h` header is removed.
    
    Reverts changelist 879756992
    
    FUTURE_COPYBARA_INTEGRATE_REVIEW=https://github.com/openxla/xla/pull/38174 from openxla:max_unroll_32 7b1b9ecb3cd68e50e873ec8a23e5d082b34f1b4c
    PiperOrigin-RevId: 880879878

--- third_party/xla/xla/tsl/concurrency/future.h
+++ third_party/xla/xla/tsl/concurrency/future.h
@@ -743,14 +743,16 @@ class Future : public internal::FutureBase<absl::StatusOr<T>> {
   }
 
   // Flattens a `Future<Future<T>>` to `Future<T>`
-  template <typename U = T, std::enable_if_t<internal::IsFuture<U>::value &&
-                                             !is_move_only>* = nullptr>
-  Future<internal::future_type_t<typename U::value_type>> Flatten() const&;
+  template <int = 0>
+  auto Flatten() const& -> std::enable_if_t<
+      internal::IsFuture<T>::value && !is_move_only,
+      Future<internal::future_type_t<typename T::value_type>>>;
 
   // Flattens a `Future<Future<T>>` to `Future<T>`
-  template <typename U = T,
-            std::enable_if_t<internal::IsFuture<U>::value>* = nullptr>
-  Future<internal::future_type_t<typename U::value_type>> Flatten() &&;
+  template <int = 0>
+  auto Flatten() && -> std::enable_if_t<
+      internal::IsFuture<T>::value,
+      Future<internal::future_type_t<typename T::value_type>>>;
 
  private:
   friend class FutureHelpers;
@@ -1271,11 +1273,11 @@ template <typename R, int&... ExplicitParameterBarrier, typename F, typename U,
 }
 
 template <typename T>
-template <typename U, std::enable_if_t<internal::IsFuture<U>::value &&
-                                       !Future<T>::is_move_only>*>
-[[nodiscard]] Future<internal::future_type_t<typename U::value_type>>
-Future<T>::Flatten() const& {
-  using R = internal::future_type_t<typename U::value_type>;
+template <int>
+auto Future<T>::Flatten() const& -> std::enable_if_t<
+    internal::IsFuture<T>::value && !Future<T>::is_move_only,
+    Future<internal::future_type_t<typename T::value_type>>> {
+  using R = internal::future_type_t<typename T::value_type>;
   auto [promise, future] = MakePromise<R>();
 
   // For const& API call we always get nested futures and values by reference.
@@ -1299,10 +1301,11 @@ Future<T>::Flatten() const& {
 }
 
 template <typename T>
-template <typename U, std::enable_if_t<internal::IsFuture<U>::value>*>
-[[nodiscard]] Future<internal::future_type_t<typename U::value_type>>
-Future<T>::Flatten() && {
-  using R = internal::future_type_t<typename U::value_type>;
+template <int>
+auto Future<T>::Flatten() && -> std::enable_if_t<
+    internal::IsFuture<T>::value,
+    Future<internal::future_type_t<typename T::value_type>>> {
+  using R = internal::future_type_t<typename T::value_type>;
   auto [promise, future] = MakePromise<R>();
 
   // For move-only futures the nested future and the value moved into the
@@ -1638,10 +1641,15 @@ class JoinStatic
 
   template <std::size_t... Is, typename... Futures>
   static void OnReady(std::shared_ptr<JoinStatic> self,
-                      std::index_sequence<Is...>, Futures... futures) {
-    (std::forward<Futures>(futures).OnReady([self](auto value) {
-      self->OnReady(std::integral_constant<size_t, Is>{}, std::move(value));
-    }),
+                      std::index_sequence<Is...>, Futures&&... futures) {
+    auto on_ready_fn = [self](auto idx, auto&& future) {
+      std::forward<decltype(future)>(future).OnReady(
+          [self, idx](auto value) mutable {
+            self->OnReady(idx, std::move(value));
+          });
+    };
+    (on_ready_fn(std::integral_constant<size_t, Is>{},
+                 std::forward<Futures>(futures)),
      ...);
   }
 
